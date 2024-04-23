9ea8aab1992df4828e2b6ed59b7c1fd577af1a85
Malay Jariwala <135534586+MalayJariwala@users.noreply.github.com>
2024-04-23 08:52:18 +0530

    Update script.js to redirect to one player and two player mods

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('onePlayerBtn').addEventListener('click', function() {
     // Redirect to the one-player game page
-    window.location.href = 'One player mode/index.html';
+    window.location.href = 'one player.html';
 });
 
 // Add event listener to the two player button
 document.getElementById('twoPlayerBtn').addEventListener('click', function() {
     // Redirect to the two-player game page
-    window.location.href = 'Two player mode/index.html';
+    window.location.href = 'Two player.html';
 });
