a28bffed63985bda2f41c636da6cff41ada0843b
Malay Jariwala <135534586+MalayJariwala@users.noreply.github.com>
2024-04-23 08:35:17 +0530

    Added tic tac toe menu

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('onePlayerBtn').addEventListener('click', function() {
+    // Redirect to the one-player game page
+    window.location.href = 'One player mode/index.html';
+});
+
+// Add event listener to the two player button
+document.getElementById('twoPlayerBtn').addEventListener('click', function() {
+    // Redirect to the two-player game page
+    window.location.href = 'Two player mode/index.html';
+});
